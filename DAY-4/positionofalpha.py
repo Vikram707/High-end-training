be5a6750d9082a05272208e36f727b2f7d780e41
Vikram Kumar <121846869+Vikram707@users.noreply.github.com>
2023-10-05 10:00:40 +0530

    Add files via upload

---
+++ DAY-4/positionofalpha.py
@@ -0,0 +1,6 @@
+a=input()
+if(ord(a)>96):
+    print(ord(a)-96)
+else:
+    print(ord(a)-64)
+    
---
+++ DAY-4/pyramid.py
@@ -0,0 +1,54 @@
+'''printing half pyramid
+a=int(input())
+for i in range(a):
+    for j in range(i+1):
+        print("*",end='')
+    print()'''
+        
+'''printing full pyrmid 
+a=int(input())
+k = 0
+for i in range(1, a+1):
+    for space in range(1, (a-i)+1):
+        print(end="  ")
+   
+    while k!=(2*i-1):
+        print("* ", end="")
+        k += 1
+   
+    k = 0
+    print()'''
+
+
+
+'''printing diamond
+
+a=int(input())
+for i in range(a):
+    print(" "*(a-i) + "* "*(i+1))
+for i in range(a):
+    print(" "*(i+1) + "* "*(a-i))'''
+
+'''printing 1 22 333 ....'''
+
+'''a=int(input())
+for i in range(a):
+    print(str(i+1)*(i+1))'''
+
+class cse:
+    x=10
+    def __init__(self):
+        self.y=30
+        def quer(self):
+            print("hi")
+    def __init__(self):
+        self.x=40
+        def ram(self)
+        print("hello")
+a=cse()
+a.ram()
+#a.quer()
+print(cse.x,a.y)
+    
+    
+    
